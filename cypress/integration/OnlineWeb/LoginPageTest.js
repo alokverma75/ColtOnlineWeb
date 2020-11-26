e9b46a724f8a0b5c57ecd18c07b467f2cd356a49
Alok Verma <alokverma75@gmail.com>
2020-11-26 19:24:38 +0530

    first commit

---
+++ cypress/integration/OnlineWeb/LoginPageTest.js
@@ -0,0 +1,25 @@
+// <reference types="Cypress" />
+
+describe('Test Login Functionality', function () {
+
+    //called after describe and before it block
+    before(function () {
+        // root-level hook
+        // runs before every test
+        cy.fixture('testdata').then(function (data) {
+            this.data = data
+        })
+    })
+
+    it('should test login functionality', function () {
+
+        cy.visit(Cypress.env('url'))
+        cy.percySnapshot('LoginPage Test');
+        cy.get("#userId").type(this.data.InternalAdmin)
+        cy.get("#password").type(this.data.password)
+        cy.get('.btnn-danger').click()
+        cy.wait(6000)
+        
+      
+    })
+})
