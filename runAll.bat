687b803f0ad76800d08b6da510d6cdcf20606e26
Alok Verma <alokverma75@gmail.com>
2020-12-05 12:50:11 +0530

    created runAll.bat to run all batches

---
+++ runAll.bat
@@ -0,0 +1,9 @@
+cd "."
+call run.bat
+cd "."
+call runChrome.bat
+cd "."
+call runEdge.bat
+cd "."
+call runFirefox.bat
+
--- runChrome.bat
+++ runChrome.bat
@@ -1,2 +1,2 @@
 set PERCY_TOKEN=8001ff496f69733608fac03b122efb4bad02328d0f7656071248b2aef49e6da7
-npx percy exec --config percy.json --  .\node_modules\.bin\cypress run --browser chrome  --spec \"cypress/integration/OnlineWeb/* \" --record --key 43618d7a-1fb6-46fd-9b2d-a4079b224091 --reporter mochawesome
+npx percy exec --config percy.json --  npx cypress run --browser chrome  --spec \"cypress/integration/OnlineWeb/* \" --record --key 43618d7a-1fb6-46fd-9b2d-a4079b224091 --reporter mochawesome
