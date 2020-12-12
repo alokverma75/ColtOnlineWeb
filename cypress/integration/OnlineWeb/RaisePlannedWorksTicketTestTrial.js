3fae623c5d2ec072a3861fc0947b408b3c2dadd2
Alok Verma <alokverma75@gmail.com>
2020-12-13 02:40:48 +0530

    added logic to select any future date in calender and corrected click logic of Calander Set button

--- cypress/integration/OnlineWeb/RaisePlannedWorksTicketTestTrial.js
+++ cypress/integration/OnlineWeb/RaisePlannedWorksTicketTestTrial.js
@@ -103,11 +103,14 @@ describe('Test Raise Planned Works Ticket Functionality', function () {
             cy.wait(2000)
             cy.get('.col-md-12 > :nth-child(2) > .ct-btn-primary').click({ force: true })
             cy.wait(5000)
-            cy.get(':nth-child(1) > [tabulator-field="checkd"] > .custom-control').click()           
+            cy.get(':nth-child(1) > [tabulator-field="checkd"] > .custom-control').click()
+           
             cy.get('.col-md-12 > :nth-child(2) > .ct-btn-primary').click({ force: true })
 
             //click on raise duplicate ticket
-            cy.get('.col > .ct-btn-primary').click()            
+            cy.get('.col > .ct-btn-primary').click()
+
+            
 
             //Page Provide details
             cy.get('#\\30 1 > .form-control').type('Test message from Alok', { force: true })
@@ -125,16 +128,97 @@ describe('Test Raise Planned Works Ticket Functionality', function () {
             //schedule date of power down
             cy.get('#start').click()
            //set button
-            cy.get('[class] [tabindex="0"]:nth-child(2)').click({ multiple: true, force: true })
-            //cy.wait(10000)
+            //cy.get('[class] [tabindex="0"]:nth-child(2)').click({ multiple: true, force: true })
+           //trail code
+            var date = new Date();
+            date.setDate(date.getDate() + 90);
+
+            var futureYear = date.getFullYear();
+            var futureMonth = date.toLocaleString("default", { month: "short" });
+            var futureDay = date.getDate();
+
+            cy.log("Future year to select: " + futureYear);
+            cy.log("Future month to select: " + futureMonth);
+            cy.log("Future day to select: " + futureDay);
+
+            
+
+            function selectMonthAndYearPowerDown() 
+            {
+                cy.get('.owl-dt-calendar-control').find('.owl-dt-calendar-control-content').then(currentDate => {
+                    cy.log("currentDate is " + currentDate.text())
+                    var futureYearFound = currentDate.text().includes(futureYear)
+                    cy.log("futureYearFound is " + futureYearFound)
+                    var futureMonthFound = currentDate.text().includes(futureMonth)
+                    cy.log("futureMonthFound is " + futureMonthFound)
+                    if (!futureYearFound) {
+                        cy.log("Inside Year loop" + !futureYearFound)
+                        cy.get('[aria-label="Next month"]').click();
+                        selectMonthAndYearPowerDown();
+                    } else if (!futureMonthFound) {
+                        cy.log("Inside Month loop" + !futureMonthFound)
+                        cy.get('[aria-label="Next month"]').click();
+                        selectMonthAndYearPowerDown();
+                    } else {
+                        cy.get('.owl-dt-calendar-cell-content').contains(futureDay).click();
+                    }
+                })
+               
+            }
+            selectMonthAndYearPowerDown();
+   
+           cy.get('.owl-dt-container-buttons > *').eq(1).should('contain', 'Set')
+           cy.get('.owl-dt-container-buttons > *').eq(1).click({ multiple: true, force: true })
+
+            //till here
+            cy.wait(5000)
             cy.get('#\\31 7 > .form-control').type('Test User', { force: true })
             cy.get('#\\31 8 > .form-control').type('+49619680252603', { force: true })
-            //cy.wait(8000)
+            cy.wait(8000)
             cy.get('#up').click({ force: true })
-          cy.get('[class] [tabindex="0"]:nth-child(2)').click({ multiple: true, force: true })
-         cy.wait(5000)
+            //cy.get('[class] [tabindex="0"]:nth-child(2)').click({ multiple: true, force: true })
+            //trail code
+            var dateUp = new Date();
+            dateUp.setDate(dateUp.getDate() + 60);
+
+            var futureYearUp = dateUp.getFullYear();
+            var futureMonthUp = dateUp.toLocaleString("default", { month: "short" });
+            var futureDayUp = dateUp.getDate();
+
+            cy.log("Future year to select: " + futureYearUp);
+            cy.log("Future month to select: " + futureMonthUp);
+            cy.log("Future day to select: " + futureDayUp);
+
+            function selectMonthAndYearPowerUp() {
+                cy.get('.owl-dt-calendar-control').find('.owl-dt-calendar-control-content').then(currentDate => {
+                    cy.log("currentDate is " + currentDate.text())
+                    var futureYearUpFound = currentDate.text().includes(futureYearUp)
+                    cy.log("futureYearUpFound is " + futureYearUpFound)
+                    var futureMonthUpFound = currentDate.text().includes(futureMonth)
+                    cy.log("futureMonthUpFound is " + futureMonthUpFound)
+                    if (!futureYearUpFound) {
+                        cy.log("Inside Year loop" + !futureYearUpFound)
+                        cy.get('[aria-label="Next month"]').click();
+                        selectMonthAndYearPowerUp();
+                    } else if (!futureMonthUpFound) {
+                        cy.log("Inside Month loop" + !futureMonthUpFound)
+                        cy.get('[aria-label="Next month"]').click();
+                        selectMonthAndYearPowerUp();
+                    } else {
+                        cy.get('.owl-dt-calendar-cell-content').contains(futureDayUp).click();
+                    }
+                })
+
+            }
+            selectMonthAndYearPowerUp();
+            cy.get('.owl-dt-container-buttons > *').eq(1).should('contain', 'Set')
+            cy.get('.owl-dt-container-buttons > *').eq(1).click({ multiple: true, force: true })
+            cy.wait(8000)
+            // //till here
+
             cy.get('#comments').type('Test Comments', { force: true })
             cy.wait(5000)
+           // cy.get('.owl-dt-container-buttons > *').eq(1).click({ multiple: true, force: true })
             //Next button
             cy.get('.btn.ct-btn-primary.ct-ml-sm.ng-star-inserted').click({ force: true })
 
